85721eda69d1314c5c3fc5130a5d148474c4edb7
paragborkar <paragborkar200@gmail.com>
2026-04-04 13:53:10 +0530

    Ping URL Added And Toast Message Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route} from "react-router-dom";
 import Auth from "./components/Auth";
 import Blogs from "./components/Blogs";
@@ -8,13 +8,24 @@ import BlogDetail from "./components/BlogDetail";
 import AddBlog from "./components/AddBlog";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { Snackbar, Alert } from "@mui/material";
 
 function App() {
+const [openToast, setOpenToast] = useState(false);
 const isLoggedIn = useSelector(state=> state.isLoggedIn);
 console.log(isLoggedIn);
  useEffect(()=>{
-  const res =  axios.get('https://mern-blogapp-backend.onrender.com/api/blog');
+  setOpenToast(true);
+  axios.get('https://mern-blogapp-backend.onrender.com/api/health').catch(err => console.log(err));
  },[])
+
+ const handleCloseToast = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenToast(false);
+  };
+
   return (
     <React.Fragment>
       <header><Header/></header>
@@ -28,6 +39,16 @@ console.log(isLoggedIn);
           <Route path="/blogs/add" element={<AddBlog/>} />
         </Routes>
       </main>
+      <Snackbar 
+        open={openToast} 
+        autoHideDuration={8000} 
+        onClose={handleCloseToast}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleCloseToast} severity="info" sx={{ width: '100%' }}>
+          Please note: The backend server is hosted on a free tier and might take a moment to start for the first time.
+        </Alert>
+      </Snackbar>
     </React.Fragment>
   );
 }
